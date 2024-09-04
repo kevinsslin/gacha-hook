9977c7a47ee516ec360211aaf98b38dd00319583
kevinsslin <kk123750964@gmail.com>
2024-09-04 15:27:06 +0800

    chore: Update solhint version and increase compiler version requirement to 0.8.24

--- test/GachaHook.t.sol
+++ test/GachaHook.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.26;
 
 import { Test } from "forge-std/Test.sol";
 
@@ -66,75 +66,6 @@ contract TestGachaHook is Test, Deployers {
         );
     }
 
-    /*
-        currentTick = 0
-        We are adding liquidity at tickLower = -60, tickUpper = 60
-
-        New liquidity must not change the token price
-
-        We saw an equation in "Ticks and Q64.96 Numbers" of how to calculate amounts of
-        x and y when adding liquidity. Given the three variables - x, y, and L - we need to set value of one.
-
-        We'll set liquidityDelta = 1 ether, i.e. ΔL = 1 ether
-        since the `modifyLiquidity` function takes `liquidityDelta` as an argument instead of 
-        specific values for `x` and `y`.
-
-        Then, we can calculate Δx and Δy:
-    Δx = Δ (L/SqrtPrice) = ( L * (SqrtPrice_tick - SqrtPrice_currentTick) ) / (SqrtPrice_tick * SqrtPrice_currentTick)
-        Δy = Δ (L * SqrtPrice) = L * (SqrtPrice_currentTick - SqrtPrice_tick)
-
-        So, we can calculate how much x and y we need to provide
-        The python script below implements code to compute that for us
-        Python code taken from https://uniswapv3book.com
-
-        ```py
-        import math
-
-        q96 = 2**96
-
-        def tick_to_price(t):
-            return 1.0001**t
-
-        def price_to_sqrtp(p):
-            return int(math.sqrt(p) * q96)
-
-        sqrtp_low = price_to_sqrtp(tick_to_price(-60))
-        sqrtp_cur = price_to_sqrtp(tick_to_price(0))
-        sqrtp_upp = price_to_sqrtp(tick_to_price(60))
-
-        def calc_amount0(liq_delta, pa, pb):
-            if pa > pb:
-                pa, pb = pb, pa
-            return int(liq_delta * q96 * (pb - pa) / pa / pb)
-
-        def calc_amount1(liq_delta, pa, pb):
-            if pa > pb:
-                pa, pb = pb, pa
-            return int(liq_delta * (pb - pa) / q96)
-
-        one_ether = 10 ** 18
-        liq = 1 * one_ether
-        eth_amount = calc_amount0(liq, sqrtp_upp, sqrtp_cur)
-        token_amount = calc_amount1(liq, sqrtp_low, sqrtp_cur)
-
-        print(dict({
-        'eth_amount': eth_amount,
-        'eth_amount_readable': eth_amount / 10**18,
-        'token_amount': token_amount,
-        'token_amount_readable': token_amount / 10**18,
-        }))
-        ```
-
-    {'eth_amount': 2995354955910434, 'eth_amount_readable': 0.002995354955910434, 'token_amount': 2995354955910412,
-    'token_amount_readable': 0.002995354955910412}
-
-        Therefore, Δx = 0.002995354955910434 ETH and Δy = 0.002995354955910434 Tokens
-
-    NOTE: Python and Solidity handle precision a bit differently, so these are rough amounts. Slight loss of precision
-    is to be expected.
-
-        */
-
     function test_addLiquidityAndSwap() public {
         // Set no referrer in the hook data
         bytes memory hookData = hook.getHookData(address(0), address(this));
@@ -151,8 +82,8 @@ contract TestGachaHook is Test, Deployers {
         uint160 sqrtPriceAtTickLower = TickMath.getSqrtPriceAtTick(-60);
         uint160 sqrtPriceAtTickUpper = TickMath.getSqrtPriceAtTick(60);
 
-        (uint256 amount0Delta,) =
-            LiquidityAmounts.getAmountsForLiquidity(SQRT_PRICE_1_1, sqrtPriceAtTickLower, sqrtPriceAtTickUpper, 1 ether);
+        (uint256 amount0Delta,) = // solhint-disable-next-line max-line-length
+         LiquidityAmounts.getAmountsForLiquidity(SQRT_PRICE_1_1, sqrtPriceAtTickLower, sqrtPriceAtTickUpper, 1 ether);
 
         modifyLiquidityRouter.modifyLiquidity{ value: amount0Delta + 1 }(
             key,
@@ -210,8 +141,8 @@ contract TestGachaHook is Test, Deployers {
         uint160 sqrtPriceAtTickLower = TickMath.getSqrtPriceAtTick(-60);
         uint160 sqrtPriceAtTickUpper = TickMath.getSqrtPriceAtTick(60);
 
-        (uint256 amount0Delta,) =
-            LiquidityAmounts.getAmountsForLiquidity(SQRT_PRICE_1_1, sqrtPriceAtTickLower, sqrtPriceAtTickUpper, 1 ether);
+        (uint256 amount0Delta,) = // solhint-disable-next-line max-line-length
+         LiquidityAmounts.getAmountsForLiquidity(SQRT_PRICE_1_1, sqrtPriceAtTickLower, sqrtPriceAtTickUpper, 1 ether);
 
         modifyLiquidityRouter.modifyLiquidity{ value: amount0Delta + 1 }(
             key,
